6810bba84ced9f7c8402d35fda8017fa36b64681
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-24 14:35:41 -0500

    Update D3Obj.java
    added more options for 3d object

--- src/D3Obj.java
+++ src/D3Obj.java
@@ -1,13 +1,20 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class D3Obj {
 
     Prism bounds;
     Color color;
+    BufferedImage image;
+    String string;
+    Font font;
+    String shape;
+
     BeanObj rendered = new BeanObj();
 
-    D3Obj(Prism bounds, Color color) {
+    D3Obj(Prism bounds, String shape, Color color) {
         this.bounds = bounds;
+        this.shape = shape;
         this.color = color;
     }
 
