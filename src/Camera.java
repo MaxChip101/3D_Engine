6a4bd5f73e9332b37214d7f986c537d4344c49aa
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-25 18:09:41 -0500

    Changed 3D positions from int to float

--- src/Camera.java
+++ src/Camera.java
@@ -2,18 +2,14 @@ public class Camera {
 
     D3 position;
     D3 rotation;
-    int fov;
-    int dts;
+    float dts;
     int renderDist;
-    int aspectRatio;
 
-    Camera(D3 position, D3 rotation, int fov, int dts, int renderDist, int aspectRatio) {
+    Camera(D3 position, D3 rotation, float dts, int renderDist) {
         this.position = position;
         this.rotation = rotation;
-        this.fov = fov;
         this.dts = dts;
         this.renderDist = renderDist;
-        this.aspectRatio = aspectRatio;
     }
 
 
