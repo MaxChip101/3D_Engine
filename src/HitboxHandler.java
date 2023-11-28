1033204c3eea1c84759141eefb6ff89370e9385b
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-28 14:50:33 -0500

    Update HitboxHandler.java

--- src/HitboxHandler.java
+++ src/HitboxHandler.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 
 public class HitboxHandler {
 
-    public ArrayList<BeanObj> objects;
+    public ArrayList<D3Obj> objects;
 
     HitboxHandler() {
         objects = new ArrayList<>();
     }
 
-    public BeanObj CheckCollisions(BeanObj object) {
+    public D3Obj CheckCollisions(D3Obj object) {
         for (int i = 0; i < objects.size(); i++) {
             if (objects.get(i) != object && object.hitbox.intersects(objects.get(i).hitbox)) {
                 return objects.get(i);
