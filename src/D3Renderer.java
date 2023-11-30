1e6af3ca68ed1c203d701bb35c447ad1d60bf18c
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-30 13:53:19 -0500

    Update D3Renderer.java

--- src/D3Renderer.java
+++ src/D3Renderer.java
@@ -37,16 +37,17 @@ public class D3Renderer {
         if (rotatedPositions.z != 0) {
             float distance = 100 * camera.dts / rotatedPositions.z;
             renderedObject.zindex = (int) distance + zindexBreak;
-            if (distance > 0 && Math.abs(distance) <= camera.renderDist) {
+            if (distance > 0) {
                 renderedObject.bounds = new Rectangle((int) ((rotatedPositions.x / 2) * camera.dts / rotatedPositions.z), (int) ((rotatedPositions.y / 2) * camera.dts / rotatedPositions.z), (int) ((object.size.x * distance / 10000) / scaleFactor), (int) ((object.size.y * distance / 10000) / scaleFactor));
                 double fixedRotation = Math.atan(renderedObject.bounds.x / (camera.dts / (camXSin / camXCos)) - renderedObject.bounds.y);
                 renderedObject.rotation = (int) (fixedRotation + object.rotation);
+                renderedObject.lineThickness = (int) ((object.lineThickness * distance / 10000) / scaleFactor);
                 if (renderedObject.font != null) {
                     renderedObject.font = new Font(object.font.getFontName(), object.font.getStyle(), (int) ((object.font.getSize() * Math.abs(distance) / 10000) / scaleFactor));
                 }
             }
         } else {
-            renderedObject.bounds = new Rectangle((int) position.x, (int) position.y, 0, 0);
+            renderedObject.bounds = new Rectangle(0, 0, 0, 0);
         }
 
         int screenWidthHalf = Main.screenWidth / 2;
@@ -56,4 +57,72 @@ public class D3Renderer {
 
         return renderedObject;
     }
+
+    public BeanObj renderLine(Camera camera, D3Line line, int zindexBreak) {
+        BeanObj renderedLine = line.rendered;
+        renderedLine.color = line.color;
+        renderedLine.shape = "ln";
+        renderedLine.rotationOffset = new Point(0, 0);
+        renderedLine.rotation = 0;
+        renderedLine.lineThickness = 0;
+
+        D3 position1 = new D3 (line.position1.x - camera.position.x, -line.position1.y - (-camera.position.y), line.position1.z - camera.position.z);
+        D3 position2 = new D3 (line.position2.x - camera.position.x, -line.position2.y - (-camera.position.y), line.position2.z - camera.position.z);
+
+        double camYSin = -Math.sin(Math.toRadians(camera.rotation.y));
+        double camYCos = -Math.cos(Math.toRadians(camera.rotation.y));
+        double camXSin = -Math.sin(Math.toRadians(camera.rotation.x));
+        double camXCos = -Math.cos(Math.toRadians(camera.rotation.x));
+        double camZSin = -Math.sin(Math.toRadians(camera.rotation.z));
+        double camZCos = -Math.cos(Math.toRadians(camera.rotation.z));
+
+        double rotatedX1 = position1.z * camYSin + position1.x * camYCos;
+        double rotatedZ1 = position1.z * camYCos - position1.x * camYSin;
+        double tempY1 = position1.y * camXCos - rotatedZ1 * camXSin;
+        double finalRotatedY1 = tempY1 * camZCos - rotatedX1 * camZSin;
+        double finalRotatedX1 = tempY1 * camZSin + rotatedX1 * camZCos;
+        double finalRotatedZ1 = position1.y * camXSin + rotatedZ1 * camXCos;
+
+        double rotatedX2 = position2.z * camYSin + position2.x * camYCos;
+        double rotatedZ2 = position2.z * camYCos - position2.x * camYSin;
+        double tempY2 = position2.y * camXCos - rotatedZ2 * camXSin;
+        double finalRotatedY2 = tempY2 * camZCos - rotatedX2 * camZSin;
+        double finalRotatedX2 = tempY2 * camZSin + rotatedX2 * camZCos;
+        double finalRotatedZ2 = position2.y * camXSin + rotatedZ2 * camXCos;
+
+
+        D3 rotatedPositions1 = new D3((int) finalRotatedX1, (int) finalRotatedY1, (int) finalRotatedZ1);
+        D3 rotatedPositions2 = new D3((int) finalRotatedX2, (int) finalRotatedY2, (int) finalRotatedZ2);
+
+        float scaleFactor = 0.01f;
+
+
+
+        if (rotatedPositions1.z != 0 || rotatedPositions2.z != 0) {
+            float distance1 = 100 * camera.dts / rotatedPositions1.z;
+            float distance2 = 100 * camera.dts / rotatedPositions2.z;
+            renderedLine.zindex = (int) (((distance1 + distance2) / 2) + zindexBreak);
+            if (distance1 > 0 && distance2 > 0) {
+                renderedLine.bounds = new Rectangle((int) ((rotatedPositions1.x / 2) * camera.dts / rotatedPositions1.z), (int) ((rotatedPositions1.y / 2) * camera.dts / rotatedPositions1.z), (int) ((rotatedPositions2.x / 2) * camera.dts / rotatedPositions2.z), (int) ((rotatedPositions2.y / 2) * camera.dts / rotatedPositions2.z));
+                if (distance1 + distance2 / 2 > 1.0f) {
+                    renderedLine.lineThickness = (int) Math.max((line.lineThickness * ((distance1 + distance2) / 2) / 10000) / scaleFactor, 0.1f);
+                } else {
+                    renderedLine.lineThickness = 0;
+                }
+            }
+        } else {
+            renderedLine.lineThickness = 0;
+            renderedLine.bounds = new Rectangle(0, 0, 0, 0);
+        }
+
+        int screenWidthHalf = Main.screenWidth / 2;
+        int screenHeightHalf = Main.screenHeight / 2;
+        renderedLine.bounds.x += screenWidthHalf;
+        renderedLine.bounds.y += screenHeightHalf;
+        renderedLine.bounds.width += screenWidthHalf;
+        renderedLine.bounds.height += screenHeightHalf;
+
+        return renderedLine;
+    }
+
 }
