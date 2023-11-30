9f4752f076f44022f08823720dff17c6397c3fa0
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-30 13:57:11 -0500

    Create D3Hitbox.java

---
+++ src/D3Hitbox.java
@@ -0,0 +1,14 @@
+public class D3Hitbox {
+
+    D3 position;
+    D3 size;
+
+    public boolean intersects(D3Hitbox hitbox) {
+        if (true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
