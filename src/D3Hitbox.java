b889b64ed53bcf0cdd750ef78a074d33ae200c54
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2024-01-12 13:20:49 -0500

    Update D3Hitbox.java

--- src/D3Hitbox.java
+++ src/D3Hitbox.java
@@ -3,8 +3,19 @@ public class D3Hitbox {
     D3 position;
     D3 size;
 
+    String name;
+
+    D3Hitbox(D3 position, D3 size, String name) {
+        this.position = position;
+        this.size = size;
+        this.name = name;
+    }
+
     public boolean intersects(D3Hitbox hitbox) {
-        if (true) {
+
+        if (position.x < hitbox.size.x && size.x > hitbox.position.x && // x
+                position.y < hitbox.size.y && size.y > hitbox.position.y && // y
+                    position.z < hitbox.size.z && size.z > hitbox.position.z)  { // z
             return true;
         } else {
             return false;
