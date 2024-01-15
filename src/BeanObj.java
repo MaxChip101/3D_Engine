573c3bcf4bd6a135f4ac16e763516c30d5574ab8
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2024-01-15 09:03:14 -0500

    added scenes

--- src/BeanObj.java
+++ src/BeanObj.java
@@ -4,8 +4,14 @@ import java.awt.image.BufferedImage;
 public class BeanObj {
 
     // Graphics
+
+        // scene variables
+        public int scene;
+
         // layer variables
         public int zindex;
+
+        //
 
         // shape variables
         public Rectangle bounds;
@@ -24,14 +30,23 @@ public class BeanObj {
         public Color color;
         public int lineThickness;
 
+        BeanObj() {
+            scene = 0;
+            zindex = 0;
+            bounds = new Rectangle(0, 0, 0, 0);
+            shape = "rect";
+            rotation = 0;
+            rotationOffset = new Point(0, 0);
+            color = new Color(0, 0, 0, 0);
+            lineThickness = 0;
+            image = BeanTools.loadImage("res/img/null.png");
+        }
 
     // Hitbox
 
         // Hitbox
         public Rectangle hitbox;
 
-        public int getZindex() {
-            return zindex;
-        }
+        public int getZindex() { return zindex; }
 
 }
