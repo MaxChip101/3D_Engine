36154b2e8c405fd0d6d9242c3bb5748b3c19940b
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2024-01-15 09:06:25 -0500

    Update D3Obj.java

--- src/D3Obj.java
+++ src/D3Obj.java
@@ -3,11 +3,13 @@ import java.awt.image.BufferedImage;
 
 public class D3Obj {
 
+    int scene;
     D3 position;
     Point size;
     D3Hitbox hitbox;
     float rotation;
     Color color;
+    float lineThickness;
     BufferedImage image;
     String string;
     Font font;
@@ -15,12 +17,14 @@ public class D3Obj {
 
     BeanObj rendered = new BeanObj();
 
-    D3Obj(D3 position, Point size, float rotation, String shape, Color color) {
+    D3Obj(D3 position, Point size, float rotation, float lineThickness, String shape, Color color, int scene) {
         this.position = position;
         this.size = size;
         this.rotation = rotation;
+        this.lineThickness = lineThickness;
         this.shape = shape;
         this.color = color;
+        this.scene = scene;
     }
 
 }
